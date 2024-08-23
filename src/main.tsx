7a6a365a5f8e0111a88d05d83df53db0abfcca8d
Tornike Tsagareishvili <dht5152@gmail.com>
2024-08-23 21:33:50 +0400

    Add: Colors For Entire Project

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "@app/sass/Breakpoints.scss";
+import "@app/sass/Global.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
