6a820b386791579ec0639d0f88d1d23488509815
Tornike Tsagareishvili <dht5152@gmail.com>
2024-08-26 18:17:02 +0400

    Add: Emoji Search Design

--- src/components/Chat/Emoji/Emoji.tsx
+++ src/components/Chat/Emoji/Emoji.tsx
@@ -1,11 +1,29 @@
-import { uuid } from "uuidv4";
+import { v4 as uuidv4 } from "uuid";
+import Emojis from "./Emojis.json";
+import rightArrow from "@app/assets/right-arrow.png";
+import leftArrow from "@app/assets/left-arrow.png";
+
 import "./Emoji.scss";
 
 export function Emoji() {
   return (
-    <div className="emoji-container">
-      <i className="em em-accept"></i>
-      <i className={`em em-accept`}></i>
+    <div className="emoji-section">
+      <form className="switcher">
+        <input type="text" placeholder="Search..." />
+        <figure>
+          <img src={leftArrow} alt="Left Arrow" />
+          <img src={rightArrow} alt="Right Arrow" />
+        </figure>
+      </form>
+      <div className="emoji-container">
+        {Emojis.map((emoji) => {
+          return (
+            <div className="emoji" key={uuidv4()}>
+              <p className={`em em-${emoji}`}></p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
