aefc20680e0e054e30dbab80ea37973c22d6c39d
Tornike Tsagareishvili <dht5152@gmail.com>
2024-08-27 15:59:33 +0400

    Drop: Remove Mapped Emoijs

--- src/components/Chat/Emoji/Emoji.tsx
+++ src/components/Chat/Emoji/Emoji.tsx
@@ -1,29 +1,11 @@
-import { v4 as uuidv4 } from "uuid";
-import Emojis from "./Emojis.json";
-import rightArrow from "@app/assets/right-arrow.png";
-import leftArrow from "@app/assets/left-arrow.png";
+import EmojiPicker from "emoji-picker-react";
 
 import "./Emoji.scss";
 
 export function Emoji() {
   return (
     <div className="emoji-section">
-      <form className="switcher">
-        <input type="text" placeholder="Search..." />
-        <figure>
-          <img src={leftArrow} alt="Left Arrow" />
-          <img src={rightArrow} alt="Right Arrow" />
-        </figure>
-      </form>
-      <div className="emoji-container">
-        {Emojis.map((emoji) => {
-          return (
-            <div className="emoji" key={uuidv4()}>
-              <p className={`em em-${emoji}`}></p>
-            </div>
-          );
-        })}
-      </div>
+      <EmojiPicker theme={"dark" as any} width={460} height={320} />
     </div>
   );
 }
