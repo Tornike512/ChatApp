23bb79523f41b76f10883618902871c80259e8a4
Tornike Tsagareishvili <dht5152@gmail.com>
2024-09-02 05:29:44 +0400

    Add: Send Button

--- src/components/Chat/ChatInput/ChatInput.tsx
+++ src/components/Chat/ChatInput/ChatInput.tsx
@@ -7,6 +7,7 @@ import importFile from "@app/assets/import-file-icon.svg";
 import importImages from "@app/assets/import-image-icon.png";
 import takePhoto from "@app/assets/take-image-icon.png";
 import recordVoice from "@app/assets/record-voice-icon.png";
+import sendIcon from "@app/assets/send-icon.png";
 
 import "./ChatInput.scss";
 
@@ -46,6 +47,7 @@ export function ChatInput() {
           placeholder="Message..."
           type="text"
         />
+        <img className="send-message" src={sendIcon} alt="Send Message" />
         <img className="import-file" src={importFile} alt="Import File" />
         <img className="import-image" src={importImages} alt="Import Images" />
         <img className="take-photo" src={takePhoto} alt="Take Photo" />
