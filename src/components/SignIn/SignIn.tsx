47362fdbbdb82d80bff34abfe678ad6f8a3c565c
Tornike Tsagareishvili <dht5152@gmail.com>
2024-09-07 13:56:25 +0400

    Add: Upload Image Style

--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,10 +1,14 @@
+import uploadImage from "@app/assets/upload-image.jpg";
+
 import "./SignIn.scss";
 
 export function SignIn() {
   return (
     <div className="sign-in-container">
       <form className="sign-in">
-        <input type="file" accept="image/*" />
+        <img src={uploadImage} alt="Upload Image" />
+        <input className="upload-input" type="file" accept="image/*" />
+        <input type="text" placeholder="Enter Your Name" />
       </form>
     </div>
   );
