43c972265271ab78862ed72244c25098f2bae808
Tornike Tsagareishvili <dht5152@gmail.com>
2024-09-08 14:03:10 +0400

    Add: Custom Input In Mixins

--- src/components/SignOrRegister/SignOrRegister.tsx
+++ src/components/SignOrRegister/SignOrRegister.tsx
@@ -7,8 +7,8 @@ export function SignOrRegister() {
   return (
     <>
       <div>
-        <Register />
-        {/* <SignIn /> */}
+        {/* <Register /> */}
+        <SignIn />
       </div>
     </>
   );
