87c3bad39952ad69ac3bb8644492c5c0cc4378a7
Tornike Tsagareishvili <dht5152@gmail.com>
2024-09-10 21:30:56 +0400

    Add: Sign In Design

--- src/components/SignOrRegister/SignIn/SignIn.tsx
+++ src/components/SignOrRegister/SignIn/SignIn.tsx
@@ -4,7 +4,8 @@ export function SignIn() {
   return (
     <div className="sign-in-container">
       <form className="sign-in">
-        <input type="text" className="Enter your name" />
+        <h2>Sign in</h2>
+        <input type="text" placeholder="Enter your name" />
         <button>Sign In</button>
       </form>
     </div>
