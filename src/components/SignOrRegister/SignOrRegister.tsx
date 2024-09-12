ca2d47ab2c19e65b640cd9d802ddc083dc56b60e
Tornike Tsagareishvili <dht5152@gmail.com>
2024-09-12 19:43:21 +0400

    Add: Sign Or Register Functional Render

--- src/components/SignOrRegister/SignOrRegister.tsx
+++ src/components/SignOrRegister/SignOrRegister.tsx
@@ -1,18 +1,43 @@
+import { useState, useEffect } from "react";
 import { Register } from "./Register";
 import { SignIn } from "./SignIn";
 
 import "./SignOrRegister.scss";
 
 export function SignOrRegister() {
+  const [showRegister, setShowRegister] = useState<boolean>(false);
+  const [showSignIn, setShowSignIn] = useState<boolean>(false);
+  const [signOrRegister, setsignOrRegister] = useState<boolean>(false);
+
+  const handleRegister = () => {
+    setShowRegister(true);
+    setsignOrRegister(false);
+  };
+
+  const handleSignIn = () => {
+    setShowSignIn(true);
+    setsignOrRegister(false);
+  };
+
+  useEffect(() => {
+    setsignOrRegister(true);
+  }, []);
+
   return (
     <>
       <div className="sign-or-register">
-        {/* <Register /> */}
-        {/* <SignIn /> */}
-        <div className="sign-or-register-container">
-          <button className="sign-in-button">Sign in</button>
-          <button className="register-button">Quick register</button>
-        </div>
+        {showRegister && <Register />}
+        {showSignIn && <SignIn />}
+        {signOrRegister && (
+          <div className="sign-or-register-container">
+            <button onClick={handleSignIn} className="sign-in-button">
+              Sign in
+            </button>
+            <button onClick={handleRegister} className="register-button">
+              Quick register
+            </button>
+          </div>
+        )}
       </div>
     </>
   );
