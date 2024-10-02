ca3998a0106fab78affab19a5e3524aea591dccd
Tornike Tsagareishvili <dht5152@gmail.com>
2024-10-02 22:58:02 +0400

    Add: Show Chat On Sign In Button Click

--- src/components/SignOrRegister/SignIn/SignIn.tsx
+++ src/components/SignOrRegister/SignIn/SignIn.tsx
@@ -1,12 +1,21 @@
+import { GlobalContext } from "@app/Providers/GlobalProvider";
+import { useContext } from "react";
+
 import "./SignIn.scss";
 
 export function SignIn() {
+  const { setJoinClicked } = useContext(GlobalContext);
+
+  const handleSignIn = () => {
+    setJoinClicked(true);
+  };
+
   return (
     <div className="sign-in-container">
       <form className="sign-in">
         <h2>Sign in</h2>
         <input type="text" placeholder="Enter your name" />
-        <button>Sign In</button>
+        <button onClick={handleSignIn}>Sign In</button>
       </form>
     </div>
   );
