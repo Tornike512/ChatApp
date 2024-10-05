1cd67d325aec6655867d1fddd7a7409ae4ff202c
Tornike Tsagareishvili <dht5152@gmail.com>
2024-10-05 20:21:13 +0400

    Add: Upload Image When Registering

--- src/components/SignOrRegister/Register/Register.tsx
+++ src/components/SignOrRegister/Register/Register.tsx
@@ -1,10 +1,13 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GlobalContext } from "@app/Providers/GlobalProvider";
 import uploadImage from "@app/assets/upload-image.jpg";
 
 import "./Register.scss";
 
 export function Register() {
+  const [registerImage, setRegisterImage] = useState<string>();
+  const [showUploadedImage, setShowUploadedImage] = useState<boolean>(false);
+
   const { setJoinClicked } = useContext(GlobalContext);
 
   const handleJoinClick = () => {
@@ -15,11 +18,34 @@ export function Register() {
     e.preventDefault();
   };
 
+  const handleImageUpload = (e: any) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const imageUrl = reader.result?.toString() || "";
+        setRegisterImage(imageUrl);
+      };
+
+      reader.readAsDataURL(file);
+    }
+
+    setShowUploadedImage(true);
+  };
+
+  console.log(registerImage);
+
   return (
     <form onSubmit={handleForm} className="register">
       <img src={uploadImage} alt="Upload Image" />
-      <input className="upload-input" type="file" accept="image/*" />
+      <input
+        onChange={handleImageUpload}
+        className="upload-input"
+        type="file"
+        accept="image/*"
+      />
       <input className="name-input" type="text" placeholder="Enter Your Name" />
+      {showUploadedImage && <img src={registerImage} alt="Uploaded Image" />}
       <button onClick={handleJoinClick} className="join">
         Join
       </button>
