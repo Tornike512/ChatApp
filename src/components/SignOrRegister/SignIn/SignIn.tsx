b1fe0007ba88d3e0e9e0ed3453c3f7598065b014
Tornike Tsagareishvili <dht5152@gmail.com>
2024-10-09 21:07:36 +0400

    Add: Username Field Handler

--- src/components/SignOrRegister/SignIn/SignIn.tsx
+++ src/components/SignOrRegister/SignIn/SignIn.tsx
@@ -1,20 +1,35 @@
 import { GlobalContext } from "@app/Providers/GlobalProvider";
-import { useContext } from "react";
+import { ReceiveAllUsers } from "@app/Hooks/ReceiveAllUsers";
+import { useContext, useState } from "react";
 
 import "./SignIn.scss";
 
 export function SignIn() {
+  const [username, setUsername] = useState<string>("");
+  const [nameValidation, setNameValidation] = useState<boolean>(false);
+
   const { setJoinClicked } = useContext(GlobalContext);
+
+  const { allUsernames } = ReceiveAllUsers();
 
   const handleSignIn = () => {
     setJoinClicked(true);
+  };
+
+  const handleUsernameField = (e: any) => {
+    setUsername(e.target.value);
   };
 
   return (
     <div className="sign-in-container">
       <form className="sign-in">
         <h2>Sign in</h2>
-        <input type="text" placeholder="Enter your name" />
+        <p className="validate-existence">Name you entered isn't registered</p>
+        <input
+          onChange={handleUsernameField}
+          type="text"
+          placeholder="Enter your name"
+        />
         <button onClick={handleSignIn}>Sign In</button>
       </form>
     </div>
