0cee7f703fb9f0529f15b9b8267eb7f36d207a93
Tornike Tsagareishvili <dht5152@gmail.com>
2024-10-18 20:43:18 +0400

    Optimize: Hook For Sending Messages To The chat

---
+++ src/Hooks/SendMessagesToChat.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export const SendMessagesToChat = () => {
+  const [messages, setMessages] = useState<string[]>([]);
+
+  const sendMessage = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/message`);
+
+      setMessages(response.data);
+    } catch (error) {
+      console.log("Error Sending Message To The Chat", error);
+    }
+  };
+
+  useEffect(() => {
+    sendMessage();
+  }, []);
+
+  return { messages };
+};
