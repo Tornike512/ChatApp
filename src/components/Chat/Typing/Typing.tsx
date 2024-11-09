e1a85427b1ac197f630b9254156f796d6363dc10
Tornike Tsagareishvili <dht5152@gmail.com>
2024-11-09 14:10:16 +0400

    Drop: Socket.io From Typing Component

--- src/components/Chat/Typing/Typing.tsx
+++ src/components/Chat/Typing/Typing.tsx
@@ -1,27 +1,6 @@
-import { useEffect, useContext } from "react";
-import { GlobalContext } from "@app/Providers/GlobalProvider";
-import useSocket from "@app/Hooks/useSocket";
-
 import "./Typing.scss";
 
 export function Typing({ userImage }: { userImage: string }) {
-  const { typingUser, setTypingUser } = useContext(GlobalContext);
-  const socket: any = useSocket();
-
-  useEffect(() => {
-    if (socket) {
-      socket.on("typing", ({ isTyping, userImage }) => {
-        console.log(isTyping, userImage);
-      });
-
-      return () => {
-        socket.off("typing");
-      };
-    }
-  }, [socket]);
-
-  console.log(typingUser);
-
   return (
     <div className="typing-container">
       <img className="user-typing" src={userImage} alt="User Image" />
