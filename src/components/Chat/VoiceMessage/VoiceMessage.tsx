2484366cfe1c44797d8579244f2dfc0bbb58e6ed
Tornike Tsagareishvili <dht5152@gmail.com>
2024-11-26 17:48:41 +0400

    Add: Voice Message Timer Style

--- src/components/Chat/VoiceMessage/VoiceMessage.tsx
+++ src/components/Chat/VoiceMessage/VoiceMessage.tsx
@@ -1,7 +1,11 @@
 import "./VoiceMessage.scss";
 
 export function VoiceMessage() {
-  return <div className="record-background"></div>;
+  return (
+    <div className="record-background">
+      <p className="timer">0:00</p>
+    </div>
+  );
 }
 
 export default VoiceMessage;
